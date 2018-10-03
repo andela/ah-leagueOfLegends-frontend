19d14b9c9d814d8d98ad6c6131ec50eb50680521
quantum-35 <quantumcomputing35@icloud.com>
2018-10-03 09:33:50 +0300

    [Chore #159965430] Remove the implementation in the login and signup reducer and action file

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,11 +1,7 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 
-import Signup from '../containers/Authentication/Signup/reducer';
-import Signin from '../containers/Authentication/Login/reducer';
 
 export default combineReducers({
-  Signup,
-  Signin,
   router: routerReducer,
 });
