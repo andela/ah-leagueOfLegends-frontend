730f32cf258cf4ba45bc11c2285102f90fa6037e
rethes <esther.mutua@strathmore.edu>
2018-10-05 00:46:03 +0300

    [Chore #159965431] Add tests for login component

---
+++ src/components/Authentication/Login/Login.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Adapter from 'enzyme-adapter-react-16';
+import Enzyme, {shallow} from "enzyme/build";
+Enzyme.configure({ adapter: new Adapter() });
+
+//logout
+describe('Logout', () => {
+    it('check local storage token does not exist', () => {
+
+    });
+    it('redirects after logout', () => {
+
+    });
+});
+//login
+describe('Login', () => {
+    it('check local storage token exists', () => {
+
+    });
+    it('redirects after login', () => {
+
+    });
+});
+//login form
+describe('<LoginForm />', () => {
+    it('render login form without crashing', () => {
+
+    });
+    it('renders error fields ', () => {
+
+    });
+    it('renders input fields of type button ', () => {
+    });
+});
