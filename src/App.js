748d82650262d1500aad151d10ea0e83e14e4bdd
Teddykavooh <teddykavooh@gmail.com>
2018-10-04 17:14:08 +0300

    [Chore #159965434]Fix route failing test and Add left semi-colon

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import { Provider } from 'react-redux';
 
 import store from './store/store';
 import "./styles/styles.css"
+import './App.css';
+import { NavLink, Switch, Route } from 'react-router-dom';
+import Home from "./views/Home";
+import Login from "./views/Login";
 
 class App extends Component {
   render() {
