e18d80d291d624c0b96712926fca6515f1fce1b0
rethes <esther.mutua@strathmore.edu>
2018-10-05 00:45:02 +0300

    [Chore #159965431] Add tests for article reducers

---
+++ src/containers/Articles/reducer.test.js
@@ -0,0 +1,30 @@
+import configureStore from 'redux-mock-store';
+
+//For passing the middlewares
+const mockStore = configureStore();
+//Init the mockstore with an empty state
+const initialState ={}
+const store = mockStore(initialState);
+//creating an actionType
+const expectedAction = type => ({
+    type,
+});
+
+describe('Reducer dispatchers for articles', () => {
+    //Add article
+    it('should dispatch an add article action', () => {
+
+    });
+    //Edit article
+    it('should dispatch an edit article action', () => {
+
+    });
+    //Delete article
+    it('should dispatch a delete article action', () => {
+
+    });
+    //List articles
+    it('should dispatch a list article action', () => {
+
+    });
+});
