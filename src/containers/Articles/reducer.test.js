d599d6b3231489a963546bf8de52716937a3f0a4
rethes <esther.mutua@strathmore.edu>
2018-10-05 13:05:38 +0300

    [Chore #159965431] Fix Eslint indentation

--- src/containers/Articles/reducer.test.js
+++ src/containers/Articles/reducer.test.js
@@ -1,30 +1,30 @@
 import configureStore from 'redux-mock-store';
 
-//For passing the middlewares
+// For passing the middlewares
 const mockStore = configureStore();
-//Init the mockstore with an empty state
-const initialState ={}
+// Init the mockstore with an empty state
+const initialState = {};
 const store = mockStore(initialState);
-//creating an actionType
+// creating an actionType
 const expectedAction = type => ({
-    type,
+  type,
 });
 
 describe('Reducer dispatchers for articles', () => {
-    //Add article
-    it('should dispatch an add article action', () => {
+  // Add article
+  it('should dispatch an add article action', () => {
 
-    });
-    //Edit article
-    it('should dispatch an edit article action', () => {
+  });
+  // Edit article
+  it('should dispatch an edit article action', () => {
 
-    });
-    //Delete article
-    it('should dispatch a delete article action', () => {
+  });
+  // Delete article
+  it('should dispatch a delete article action', () => {
 
-    });
-    //List articles
-    it('should dispatch a list article action', () => {
+  });
+  // List articles
+  it('should dispatch a list article action', () => {
 
-    });
+  });
 });
