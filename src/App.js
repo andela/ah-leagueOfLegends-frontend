6af08f820a091bb26dba21bff47664001cc07fe4
Derrick Korir <derrick.korir@gmail.com>
2018-10-10 15:32:03 +0300

    [Feature #159965439] Render Navbar and Login

--- src/App.js
+++ src/App.js
@@ -5,8 +5,13 @@ import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
 import store from './store/store';
 import ROUTE from './utils/routes';
 import Articles from './containers/Articles/Read';
-import Navbar from './components/Authentication/Navbar';
+// import Navbar from './components/Authentication/Navbar';
 import Register from './components/Authentication/Signup/Register';
+
+
+import Login from './containers/Authentication/Login/Login';
+import Navbar from './components/Navbar';
+
 
 class App extends Component {
   componentDidMount() {}
@@ -16,8 +21,9 @@ class App extends Component {
       <Provider store={store}>
         <Router>
           <div>
-            <Navbar />
             <Register />
+            <Navbar />
+            <Login />
             <Switch>
               <Route exact path={ROUTE.home} component={Articles} />
             </Switch>
