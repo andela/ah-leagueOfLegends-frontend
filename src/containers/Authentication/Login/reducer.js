dd562a56e6c1d3854649a631bda28a039e3778fc
Derrick Korir <derrick.korir@gmail.com>
2018-10-10 15:12:25 +0300

    [Feature #159965439] Add login reducer

---
+++ src/containers/Authentication/Login/reducer.js
@@ -0,0 +1,27 @@
+
+
+const initialState = {
+    isAuthenticated: false
+};
+
+export const loginReducer = (state=initialState, action) => {
+    switch(action.type){
+        case 'USERS_LOGIN_REQUEST':
+            return {
+                
+            }
+        case 'USERS_LOGIN_SUCCESS':
+            return {
+                ...state,
+                isAuthenticated: true,
+                user:  action.payload
+            }
+        case 'USERS_LOGIN_FAILURE':
+            return {
+                ...state,
+                error: action.error
+            }
+        default:
+            return state
+    }
+}
