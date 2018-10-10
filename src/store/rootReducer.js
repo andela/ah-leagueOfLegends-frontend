ad2312a991861b3ba945cce81e8cdbd2f3af45be
Derrick Korir <derrick.korir@gmail.com>
2018-10-10 15:16:13 +0300

    [Feature #159965439] Add Login reducer to root reducer

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
+import { loginReducer } from '../containers/Authentication/Login/reducer';
 
 import articles from '../containers/Articles/Read/reducer';
 import authReducer from '../components/Authentication/Signup/reducers/authReducer';
@@ -9,4 +10,5 @@ export default combineReducers({
   articles,
   authReducer,
   router: routerReducer,
+  Login: loginReducer
 });
