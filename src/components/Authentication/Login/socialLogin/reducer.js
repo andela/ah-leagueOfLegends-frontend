6d79605651bace1f13e37f51d09c32d659c26d6a
derkip <dkip64@gmail.com>
2018-10-11 13:16:59 +0300

    [Feature #159965440] Add reducers

---
+++ src/components/Authentication/Login/socialLogin/reducer.js
@@ -0,0 +1,21 @@
+const initialState = {
+    isAuthenticated: false
+};
+
+export const SocialLoginReducer = (state=initialState, action) => {
+    switch(action.type){
+        case 'USERS_LOGIN_SUCCESS':
+            return {
+                ...state,
+                isAuthenticated: true,
+                user:  action.payload
+            }
+        case 'USERS_LOGIN_FAILURE':
+            return {
+                ...state,
+                error: action.error
+            }
+        default:
+            return state
+    }
+}
