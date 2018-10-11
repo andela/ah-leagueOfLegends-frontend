78d3de748e422fbbf8edb32c0f576375d5e514de
Derrick Korir <derrick.korir@gmail.com>
2018-10-11 12:25:21 +0300

    [Feature #159965439] Make changes to home page

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
-const Home = () => (
-  <div className="home">
-    <h1>Welcome to Authors Haven</h1>
-    <p> Feel free to browse around and learn more about me.</p>
-  </div>
-);
+const Home = () => {
+  return(
+    <div>
+      <div className="home">
+        <h1>Welcome to Authors Haven</h1>
+        <p> Feel free to browse around and learn more about me.</p>
+      </div>
+    </div>
+
+  );
+
+};
 
 export default Home;
