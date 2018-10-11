67cf0acb93e4c0e60bf930161f4d541580105816
Derrick Korir <derrick.korir@gmail.com>
2018-10-11 12:20:56 +0300

    [Feature #159965439] Add Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Navbar extends Component {
     render () {
+        console.log(this.props.state)
+        const { isAuthenticated } = this.props.state
+        console.log(isAuthenticated)
         return (
             <div>
             <div className="navbar-fixed">
@@ -11,15 +15,23 @@ class Navbar extends Component {
                         <a href="/" className="brand-logo black-text">
                         Authors Haven</a>
                         <a href="/" data-target="mobile-nav" className="sidenav-trigger">
-                        {/* <i className="material-icons black-text">menu</i> */}
                         </a>
+                        {(isAuthenticated) ? (
+                        <ul className="right hide-on-med-and-down grey-text">
+                            <li><i className="material-icons">search</i></li>
+                            <li><i className="material-icons">notifications_none</i></li>
+                            <li><i className="material-icons">bookmark_border</i></li>
+                        </ul>) :(
                         <ul className="right hide-on-med-and-down grey-text">
                             <li><i className="material-icons">search</i></li> 
                             <li><a className="waves-effect waves-light btn white teal-text
                                 modal-trigger" href="#modal1">Sign in</a></li>
                             <li><button className="waves-effect waves-light btn white teal-text
-                                modal-trigger" href='#modal2'>Get started</button></li>  
-                        </ul>
+                                modal-trigger" href='#modal2'>Get started</button></li>
+                        </ul>)}
+                        
+
+
                     </div>
                     </div>
                 </nav>
@@ -29,4 +41,10 @@ class Navbar extends Component {
     }
 }
 
-export default Navbar;
+const mapStateToProps = (state) => {
+    return {
+       state: state.Login
+    };
+  }
+  
+  export  default connect(mapStateToProps)(Navbar);
