01c94dd3d1c2ea30313a65ab750f90bfb03d9341
rethes <esther.mutua@strathmore.edu>
2018-10-12 13:34:20 +0300

    [Feature #159965441] Add routes file

---
+++ src/routes.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import {
+    NavLink, Switch, Route, BrowserRouter as Router,
+} from 'react-router-dom';
+
+import store from './store/store';
+import ROUTES from './utils/routes';
+import Home from './containers/Home';
+
+import ViewProfile from "./containers/Profile/ViewProfile/index";
+import UpdateProfile from "./containers/Profile/UpdateProfile/index";
+
+export default () => (
+    <Provider store={store}>
+        <Router>
+
+                <Switch>
+                    <Route exact path={ROUTES.home} component={Home} />
+
+                    <Route exact path={ROUTES.getUserProfile} component={ViewProfile} />
+
+                    <Route exact path={ROUTES.updateUserProfile} component={UpdateProfile} />
+
+                </Switch>
+
+        </Router>
+    </Provider>
+);
