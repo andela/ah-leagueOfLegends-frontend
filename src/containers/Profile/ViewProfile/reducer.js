ce33aceeb5b5dfa81ef5e1cf2d569eb4cba1383a
rethes <esther.mutua@strathmore.edu>
2018-10-12 13:32:08 +0300

    [Feature #159965441] Add view profile reducers

---
+++ src/containers/Profile/ViewProfile/reducer.js
@@ -0,0 +1,47 @@
+import { VIEW_PROFILE_REQUEST, VIEW_PROFILE_FAILURE,
+    VIEW_PROFILE_SUCCESS } from './ActionTypes';
+
+const initialState = {
+    isFetching:false,
+    payload:{
+        profile:{
+        username: "",
+        bio: "...",
+        image: ""
+        }
+    },
+    success:false,
+
+    failure:false,
+    errors:null,
+};
+
+const viewProfileReducer = (state = initialState, action) => {
+    const {type, payload, errors} = action;
+    switch(type) {
+        case VIEW_PROFILE_REQUEST:
+            return { ...state,
+                isFetching: true };
+        case VIEW_PROFILE_FAILURE:
+            return {
+                ...state,
+                isFetching: false,
+                failure: true,
+                success: false,
+                errors,
+            };
+        case VIEW_PROFILE_SUCCESS:
+            return {
+
+                isFetching: false,
+                failure: false,
+                success: true,
+                errors:null,
+                payload,
+            };
+        default:
+            return state;
+    }
+};
+
+export default viewProfileReducer;
