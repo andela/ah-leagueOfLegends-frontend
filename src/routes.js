48da6e258ceb65921adc522a58d47c45dba86221
rethes <esther.mutua@strathmore.edu>
2018-10-12 13:35:40 +0300

    [Feature #159965441] Remove unused imports

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Provider } from 'react-redux';
 import {
-    NavLink, Switch, Route, BrowserRouter as Router,
+    Switch, Route, BrowserRouter as Router,
 } from 'react-router-dom';
 
 import store from './store/store';
@@ -14,16 +14,13 @@ import UpdateProfile from "./containers/Profile/UpdateProfile/index";
 export default () => (
     <Provider store={store}>
         <Router>
-
                 <Switch>
                     <Route exact path={ROUTES.home} component={Home} />
 
                     <Route exact path={ROUTES.getUserProfile} component={ViewProfile} />
 
                     <Route exact path={ROUTES.updateUserProfile} component={UpdateProfile} />
-
                 </Switch>
-
         </Router>
     </Provider>
 );
