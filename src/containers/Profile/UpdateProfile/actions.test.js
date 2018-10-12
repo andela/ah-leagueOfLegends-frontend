a1a0fe6526145a4746f8cbe7211f005522ff2a45
rethes <esther.mutua@strathmore.edu>
2018-10-12 13:46:40 +0300

    [Feature #159965441] Update update profile action tests

--- src/containers/Profile/UpdateProfile/actions.test.js
+++ src/containers/Profile/UpdateProfile/actions.test.js
@@ -1,34 +1,38 @@
 import * as actions from './actions';
 import {} from "./ActionTypes";
+import { UPDATE_USER_PROFILE_REQUEST } from "./ActionTypes";
+import { VIEW_PROFILE_FAILURE } from "../ViewProfile/ActionTypes";
+import { UPDATE_PROFILE_SUCCESS } from "./ActionTypes";
+import { UPDATE_PROFILE_FAILURE } from "./ActionTypes";
 
 describe('Actions for edit profile', () => {
     // Create edit profile request
-    it('should do a fetch user profile request action', () => {
+    it('should do an update user profile request action', () => {
         const expectedAction = {
-            type: VIEW_PROFILE_REQUEST,
+            type: UPDATE_USER_PROFILE_REQUEST,
         };
-        expect(actions.fetchUserProfile()).toEqual(expectedAction);
+        expect(actions.updateUserProfile()).toEqual(expectedAction);
     });
 
-    // Dispatch error when request fails
-    it('should dispatch errors when the request fails', () => {
+    // Dispatch error when update user request fails
+    it('should dispatch errors when the update request fails', () => {
         const errors = {
             errors: {
-                error: ['The requested profile does not exist.'],
+                error: ['Profile not updated.'],
             },
         };
         const expectedAction = {
-            type: VIEW_PROFILE_FAILURE,
+            type: UPDATE_PROFILE_FAILURE,
             errors,
         };
-        expect(actions.fetchUserProfileFailure(errors)).toEqual(expectedAction);
+        expect(actions.updateUserProfileFailure(errors)).toEqual(expectedAction);
     });
 
-    //Dispatch payload when the request is a success
-    it('should dispatch the payload when the request is a success', () => {
+    //Dispatch payload when the update user request is a success
+    it('should dispatch the payload when the update user request is a success', () => {
         const payload = {
             "profile": {
-                "username": "Takeda",
+                "username": "Takedah",
                 "bio": "",
                 "image": "https://api.adorable.io/avatars/500/49991232",
                 "date_joined": "2018-09-21 12:06:41.129807+00:00",
@@ -40,10 +44,10 @@ describe('Actions for edit profile', () => {
             }
         };
         const expectedAction = {
-            type: VIEW_PROFILE_SUCCESS,
+            type: UPDATE_PROFILE_SUCCESS,
             payload,
         };
-        expect(actions.fetchUserProfileSuccess(payload)).toEqual(expectedAction);
+        expect(actions.updateUserProfileSuccess(payload)).toEqual(expectedAction);
     });
 
 });
