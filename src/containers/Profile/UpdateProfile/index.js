398b078989623d614308ae59fc5397b3acf0de45
rethes <esther.mutua@strathmore.edu>
2018-10-12 13:30:46 +0300

    [Feature #159965441] Add update profile index file

---
+++ src/containers/Profile/UpdateProfile/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const UpdateProfile = props => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+UpdateProfile.propTypes = {
+    
+};
+
+export default UpdateProfile;
