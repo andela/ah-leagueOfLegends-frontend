6125ef51337e9975a5d7332cb6159430f78b800c
rethes <esther.mutua@strathmore.edu>
2018-10-15 23:13:00 +0300

    [Feature #159965441] Fix
    Signed-off-by: rethes <esther.mutua@strathmore.edu>

---
+++ src/components/ProfileHeader/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ProfileHeader = ({ image }) => (
+  <div>
+    <header>
+      <div className="navbar-fixed ">
+        <nav className="white z-depth-0">
+          <div className="container">
+            <div className="nav-wrapper">
+              <a href="#" className="brand-logo col s12 black-text">Authors Haven</a>
+              <a href="#" data-target="mobile-nav" className="sidenav-trigger">
+                <i className="material-icons black-text">menu</i>
+              </a>
+              <ul className="right hide-on-med-and-down grey-text">
+                <li><i className="material-icons">search</i></li>
+                <li><i className="material-icons">notifications_none</i></li>
+                <li><i className="material-icons">bookmark_border</i></li>
+                <li><img className="small-profile" src={image} alt="header-avatar" /></li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
+      <ul className="sidenav" id="mobile-nav">
+        <li><a href="">Home</a></li>
+        <li><a href="">Search</a></li>
+        <li><a href="">Profile</a></li>
+        <li><a href="">Home</a></li>
+      </ul>
+    </header>
+
+  </div>
+);
+
+ProfileHeader.propTypes = { image: PropTypes.string.isRequired };
+
+export default ProfileHeader;
