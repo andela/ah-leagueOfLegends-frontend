54d1b6d91d7ed49728eb556ea8bcd3a3e72c5ba8
rethes <esther.mutua@strathmore.edu>
2018-10-15 22:31:26 +0300

    [Feature #159965441] Fix linting to indent of 2
    Signed-off-by: rethes <esther.mutua@strathmore.edu>

--- src/containers/Profile/ViewProfile/actions.js
+++ src/containers/Profile/ViewProfile/actions.js
@@ -1,37 +1,36 @@
-import { VIEW_PROFILE_REQUEST, VIEW_PROFILE_FAILURE,
-    VIEW_PROFILE_SUCCESS } from './ActionTypes';
 import axios from 'axios';
+import {
+  VIEW_PROFILE_REQUEST, VIEW_PROFILE_FAILURE,
+  VIEW_PROFILE_SUCCESS,
+} from './ActionTypes';
 
-export const fetchUserProfile = () => ({
-    type: VIEW_PROFILE_REQUEST,
+export const fetchUserProfile = () => ({ type: VIEW_PROFILE_REQUEST });
+
+export const fetchUserProfileSuccess = payload => ({
+  type: VIEW_PROFILE_SUCCESS,
+  payload,
 });
 
-export const fetchUserProfileSuccess = (payload) => ({
-    type: VIEW_PROFILE_SUCCESS,
-    payload:payload,
+export const fetchUserProfileFailure = errors => ({
+  type: VIEW_PROFILE_FAILURE,
+  errors,
 });
 
-export const fetchUserProfileFailure = (errors) => ({
-    type: VIEW_PROFILE_FAILURE,
-    errors,
-});
-
-//fetch user details
-const fetchUserDetails  = (username) => {
-    let url = `https://ah-leagueoflegends-staging.herokuapp.com/api/profiles/${username}`;
-    return (dispatch) => {
-        dispatch(fetchUserProfile());
-        return axios.get(url).then(
-            (response) => {
-                let userData = response.data;
-                dispatch(fetchUserProfileSuccess(userData))
-            },
-            (errors) => {
-                dispatch(fetchUserProfileFailure(errors))
-            }
-        )
-
-    }
+// fetch user details
+const fetchUserDetails = (username) => {
+  const url = `https://ah-leagueoflegends-staging.herokuapp.com/api/profiles/${username}`;
+  return (dispatch) => {
+    dispatch(fetchUserProfile());
+    return axios.get(url).then(
+      (response) => {
+        const userData = response.data;
+        dispatch(fetchUserProfileSuccess(userData));
+      },
+      (errors) => {
+        dispatch(fetchUserProfileFailure(errors));
+      },
+    );
+  };
 };
 
 export default fetchUserDetails;
