33760be69afda31f2f3b9ec0e8476b2d9c9198e0
rethes <esther.mutua@strathmore.edu>
2018-10-15 22:32:36 +0300

    [Feature #159965441] Fix linting to use indent of 2
    Signed-off-by: rethes <esther.mutua@strathmore.edu>

--- src/containers/Profile/ViewProfile/actions.test.js
+++ src/containers/Profile/ViewProfile/actions.test.js
@@ -1,53 +1,46 @@
-import  * as actions from '../UpdateProfile/actions';
+import * as actions from '../UpdateProfile/actions';
 import {
-    UPDATE_PROFILE_FAILURE,
-    UPDATE_PROFILE_SUCCESS,
-    UPDATE_USER_PROFILE_REQUEST
-} from "../UpdateProfile/ActionTypes";
+  UPDATE_PROFILE_FAILURE,
+  UPDATE_PROFILE_SUCCESS,
+  UPDATE_USER_PROFILE_REQUEST,
+} from '../UpdateProfile/ActionTypes';
 
 describe('Actions for profile', () => {
   // Create update user profile request
   it('should do an update user profile request action', () => {
-      const expectedAction = {
-          type: UPDATE_USER_PROFILE_REQUEST,
-      };
-      expect(actions.updateUserProfile()).toEqual(expectedAction);
+    const expectedAction = { type: UPDATE_USER_PROFILE_REQUEST };
+    expect(actions.updateUserProfile()).toEqual(expectedAction);
   });
 
   // Dispatch error when update user profile request fails
   it('should dispatch errors when the update user profile request fails', () => {
-      const errors = {
-          errors: {
-              error: ['Profile failed to update'],
-          },
-      };
-      const expectedAction = {
-          type: UPDATE_PROFILE_FAILURE,
-          errors,
-      };
-      expect(actions.updateUserProfileFailure(errors)).toEqual(expectedAction);
+    const errors = { errors: { error: ['Profile failed to update'] } };
+    const expectedAction = {
+      type: UPDATE_PROFILE_FAILURE,
+      errors,
+    };
+    expect(actions.updateUserProfileFailure(errors)).toEqual(expectedAction);
   });
 
-  //Dispatch payload when the update user profile request is a success
+  // Dispatch payload when the update user profile request is a success
   it('should dispatch the payload when the update user profile request is a success', () => {
-        const payload = {
-            "profile": {
-                "username": "Takeda",
-                "bio": "",
-                "image": "https://api.adorable.io/avatars/500/49991232",
-                "date_joined": "2018-09-21 12:06:41.129807+00:00",
-                "following": false,
-                "bookmarks": [
-                    "Ants are cool",
-                    "Gone Girl"
-                ]
-            }
-        };
-        const expectedAction = {
-            type: UPDATE_PROFILE_SUCCESS,
-            payload,
-        };
-        expect(actions.updateUserProfileSuccess(payload)).toEqual(expectedAction);
-    });
-
+    const payload = {
+      profile: {
+        username: 'Takeda',
+        bio: '',
+        image: 'https://api.adorable.io/avatars/500/49991232',
+        date_joined: '2018-09-21 12:06:41.129807+00:00',
+        following: false,
+        bookmarks: [
+          'Ants are cool',
+          'Gone Girl',
+        ],
+      },
+    };
+    const expectedAction = {
+      type: UPDATE_PROFILE_SUCCESS,
+      payload,
+    };
+    expect(actions.updateUserProfileSuccess(payload)).toEqual(expectedAction);
+  });
 });
--- src/containers/Profile/ViewProfile/reducer.js
+++ src/containers/Profile/ViewProfile/reducer.js
@@ -1,46 +1,50 @@
-import { VIEW_PROFILE_REQUEST, VIEW_PROFILE_FAILURE,
-    VIEW_PROFILE_SUCCESS } from './ActionTypes';
+import {
+  VIEW_PROFILE_REQUEST, VIEW_PROFILE_FAILURE,
+  VIEW_PROFILE_SUCCESS,
+} from './ActionTypes';
 
 const initialState = {
-    isFetching:false,
-    payload:{
-        profile:{
-        username: "",
-        bio: "",
-        image: ""
-        }
+  isFetching: false,
+  payload: {
+    profile: {
+      username: '',
+      bio: '',
+      image: '',
     },
-    success:false,
-    failure:false,
-    errors:null,
+  },
+  success: false,
+  failure: false,
+  errors: null,
 };
 
 const viewProfileReducer = (state = initialState, action) => {
-    const {type, payload, errors} = action;
-    switch(type) {
-        case VIEW_PROFILE_REQUEST:
-            return { ...state,
-                isFetching: true };
-        case VIEW_PROFILE_FAILURE:
-            return {
-                ...state,
-                isFetching: false,
-                failure: true,
-                success: false,
-                errors,
-            };
-        case VIEW_PROFILE_SUCCESS:
-            return {
+  const { type, payload, errors } = action;
+  switch (type) {
+    case VIEW_PROFILE_REQUEST:
+      return {
+        ...state,
+        isFetching: true,
+      };
+    case VIEW_PROFILE_FAILURE:
+      return {
+        ...state,
+        isFetching: false,
+        failure: true,
+        success: false,
+        errors,
+      };
+    case VIEW_PROFILE_SUCCESS:
+      return {
 
-                isFetching: false,
-                failure: false,
-                success: true,
-                errors:null,
-                payload,
-            };
-        default:
-            return state;
-    }
+        isFetching: false,
+        failure: false,
+        success: true,
+        errors: null,
+        payload,
+      };
+    default:
+      return state;
+  }
 };
 
 export default viewProfileReducer;
