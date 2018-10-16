87240f9ca07a4fed36a15060e871bfdfd70ddf19
rethes <esther.mutua@strathmore.edu>
2018-10-16 16:28:00 +0300

    [Feature #159965441] Add Not Found Component
    Signed-off-by: rethes <esther.mutua@strathmore.edu>

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -3,3 +3,25 @@ import React from 'react';
 const notFound = () => <div><h1>NOT FOUND</h1></div>;
 
 export default notFound;
+import PropTypes from 'prop-types';
+import ViewProfile from '../ViewProfiles';
+
+const NotFound = ({ item }) => (
+  <main className="conatiner">
+    <div className="row">
+      <div className="col m5 s12 offset-m4 profile">
+        <div className="user-info">
+          <h1>
+            {item}
+            {' '}
+            Not Found
+          </h1>
+        </div>
+      </div>
+    </div>
+  </main>
+);
+
+ViewProfile.propTypes = { item: PropTypes.string.isRequired };
+
+export default NotFound;
