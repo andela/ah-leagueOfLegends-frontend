eeb93aef9c33fd4a55843ab02e994559599d1629
rethes <esther.mutua@strathmore.edu>
2018-10-16 07:45:56 +0300

    [Feature #159965441] Add update profile proptypes
    Signed-off-by: rethes <esther.mutua@strathmore.edu>

--- src/containers/Profile/UpdateProfile/index.js
+++ src/containers/Profile/UpdateProfile/index.js
@@ -1,113 +1,110 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import connect from "react-redux/es/connect/connect";
-import { bindActionCreators } from "redux";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import connect from 'react-redux/es/connect/connect';
+import { bindActionCreators } from 'redux';
 import toastr from 'toastr';
 
 import updateUser from './actions';
-import {Image} from 'cloudinary-react';
-import fetchUserDetails from "../ViewProfile/actions";
-import Header from "../../../components/ProfileHeader";
-import EditProfile from "../../../components/EditProfile";
-import cloudinary from 'cloudinary-core';
+import fetchUserDetails from '../ViewProfile/actions';
+import Header from '../../../components/ProfileHeader';
+import EditProfile from '../../../components/EditProfile';
 
 class UpdateProfile extends Component {
   constructor() {
     super();
     this.state = {
-      email: "",
-      username: "",
-      bio: "",
-      image: "",
-      selectedFile: null};
+      username: '',
+      bio: '',
+      image: '',
+      selectedFile: null,
+    };
     this.handleSave = this.handleSave.bind(this);
     this.handleCancel = this.handleCancel.bind(this);
     this.handleChange = this.handleChange.bind(this);
   }
+
   componentDidMount() {
     this.props.fetchUserDetails(this.props.match.params.username)
-      .catch(error => {
-        toastr.error(error)});
-
+      .catch((error) => {
+        toastr.error(error);
+      });
   }
+
   componentWillReceiveProps(nextProps) {
     const { username, bio, image } = nextProps.viewProfileReducer.payload.profile;
-    this.setState({ username, bio, image })
+    this.setState({ username, bio, image });
   }
 
   handleChange(e) {
     const { value } = e.target;
+    this.setState({ [e.target.id]: value });
+  }
 
-
-    this.setState({
-      [e.target.id]: value,
-    });
-  };
-  handleSave(e) {
+  handleSave() {
     const { username, bio } = this.state;
-    const profile ={
+    const profile = {
       user: {
         username,
-        bio
-      }
+        bio,
+      },
     };
     this.props.updateUser(profile)
       .then(() => {
         toastr.success('Profile Update Success');
-        const {payload,} = this.props.viewProfileReducer;
-        const username = ( profile.user.username === '') ? payload.profile.username :profile.user.username;
+        const { payload } = this.props.viewProfileReducer;
+        const username = (profile.user.username === '') ? payload.profile.username : profile.user.username;
         this.props.history.push(`/profile/${username}`);
-      }).catch(error => {
-      toastr.error(error);
-    });
+      }).catch((error) => {
+        toastr.error(error);
+      });
   }
+
   handleCancel(event) {
     event.preventDefault();
     const username = this.props.match.params.username;
     this.props.history.replace(`/profile/${username}`);
   }
+
   render() {
-    const {payload,} = this.props.viewProfileReducer;
+    const { payload } = this.props.viewProfileReducer;
 
     return (
       <div>
-        <Header username={payload.profile.username} image={payload.profile.image}/>
-        <EditProfile username={this.state.username}
-                     bio={this.state.bio}
-                     image={this.state.image}
-                     onChange={this.handleChange}
-                     onSave={this.handleSave}
-                     onCancel={this.handleCancel}/>
-
+        <Header username={payload.profile.username} image={payload.profile.image} />
+        <EditProfile
+          username={this.state.username}
+          bio={this.state.bio}
+          image={this.state.image}
+          onChange={this.handleChange}
+          onSave={this.handleSave}
+          onCancel={this.handleCancel}
+        />
       </div>
-
-
     );
   }
 }
 
 UpdateProfile.propTypes = {
   username: PropTypes.string.isRequired,
-  bio: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired,
-  onChange: PropTypes.func.isRequired,
-  onSave: PropTypes.func.isRequired,
-  onCancel: PropTypes.func.isRequired,
+  match: PropTypes.object.isRequired,
+  viewProfileReducer: PropTypes.object.isRequired,
+  updateUser: PropTypes.func.isRequired,
+  history: PropTypes.object.isRequired,
+  fetchUserDetails: PropTypes.func.isRequired,
 
 };
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   viewProfileReducer: state.viewProfileReducer,
-  editProfileReducer: state.editProfileReducer
+  editProfileReducer: state.editProfileReducer,
 });
 
-const matchDispatchToProps = (dispatch) =>
-  bindActionCreators(
-    {
-      fetchUserDetails: fetchUserDetails,
-      updateUser: updateUser
-    }, dispatch);
+const matchDispatchToProps = dispatch => bindActionCreators(
+  {
+    fetchUserDetails,
+    updateUser,
+  }, dispatch);
 
 export default connect(
-  mapStateToProps,matchDispatchToProps
+  mapStateToProps, matchDispatchToProps,
 )(UpdateProfile);
