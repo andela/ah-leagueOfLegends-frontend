453f464ecbc1bb86a3c0bc88a31fefb79e39312b
rethes <esther.mutua@strathmore.edu>
2018-10-16 16:28:28 +0300

    [Feature #159965441] Update edit avatar image with an icon
    Signed-off-by: rethes <esther.mutua@strathmore.edu>

--- src/components/ViewProfiles/index.js
+++ src/components/ViewProfiles/index.js
@@ -2,16 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const ViewProfile = ({
-  username, image, bio, onEditClick,
+  username, image, bio, onEditClick, avatarUpload,
 }) => (
   <main className="conatiner">
     <div className="row">
-      <div className="col m6 s12 offset-m3 profile">
+      <div className="col m5 s12 offset-m3 profile">
         <div className="user-info">
           <div className="user-detail">
             <h4 className="username">{username}</h4>
             <button
               data-target="modal1"
+              type="submit"
               className="waves-effect waves-light btn  btn-flat white grey-text"
               onClick={onEditClick}
             >
@@ -20,13 +21,24 @@ const ViewProfile = ({
           </div>
           <p className="bio offset-s2">{bio}</p>
           <ul className="followers-link">
-            <li><a className="grey-text p-r-30" href="#"> 0 Following</a></li>
-            <li><a className="grey-text" href="#"> 0 Followers</a></li>
+            <li>
+              <a className="grey-text p-r-30" href="#">
+                0 Following
+              </a>
+            </li>
+            <li>
+              <a className="grey-text" href="#">
+                0 Followers
+              </a>
+            </li>
           </ul>
         </div>
-        <div className="avatar-wrapper">
-          <img className="avatar" src={image} alt="header-profile" />
-        </div>
+      </div>
+      <div className="col m1  avatar-wrapper">
+        <img className="avatar" src={image} alt="header-profile" />
+        <button onClick={avatarUpload} type="submit">
+          <i className="material-icons">add_a_photo</i>
+        </button>
       </div>
     </div>
   </main>
@@ -37,6 +49,7 @@ ViewProfile.propTypes = {
   bio: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired,
   onEditClick: PropTypes.func.isRequired,
+  avatarUpload: PropTypes.func.isRequired,
 };
 
 export default ViewProfile;
