26878d35dbdb4b0aa0b427100af2805274fbfa32
rethes <esther.mutua@strathmore.edu>
2018-10-16 16:50:07 +0300

    [Feature #159965441] Update file indentation
    Signed-off-by: rethes <esther.mutua@strathmore.edu>

--- src/routes.js
+++ src/routes.js
@@ -1,26 +1,24 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import {
-    Switch, Route, BrowserRouter as Router,
-} from 'react-router-dom';
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
 
 import store from './store/store';
 import ROUTES from './utils/routes';
 import Home from './containers/Home';
 
-import ViewProfile from "./containers/Profile/ViewProfile/index";
-import UpdateProfile from "./containers/Profile/UpdateProfile/index";
+import ViewProfile from './containers/Profile/ViewProfile/index';
+import UpdateProfile from './containers/Profile/UpdateProfile/index';
 
 export default () => (
-    <Provider store={store}>
-        <Router>
-                <Switch>
-                    <Route exact path={ROUTES.home} component={Home} />
+  <Provider store={store}>
+    <Router>
+      <Switch>
+        <Route exact path={ROUTES.home} component={Home} />
 
-                    <Route exact path={ROUTES.getUserProfile} component={ViewProfile} />
+        <Route exact path={ROUTES.getUserProfile} component={ViewProfile} />
 
-                    <Route exact path={ROUTES.updateUserProfile} component={UpdateProfile} />
-                </Switch>
-        </Router>
-    </Provider>
+        <Route exact path={ROUTES.updateUserProfile} component={UpdateProfile} />
+      </Switch>
+    </Router>
+  </Provider>
 );
