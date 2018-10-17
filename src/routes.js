db169cb3fd0d93fbc823fad97d75220b7ca73f17
rethes <esther.mutua@strathmore.edu>
2018-10-17 03:31:51 +0300

    [Feature #159965441] Add article routes
    Signed-off-by: rethes <esther.mutua@strathmore.edu>

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,12 @@ import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
 
 import store from './store/store';
 import ROUTES from './utils/routes';
-import Home from './containers/Home';
+import Articles from './containers/Articles/Read';
+import Success from './components/Authentication/Signup/Success';
+import NewArticle from './containers/Articles/Create';
+import MainArticle from './containers/Articles/Article/largArticle';
+import UpdateArticle from './containers/Articles/Update/index';
+import PrivateRoute from './utils/PrivateRoute';
 
 import ViewProfile from './containers/Profile/ViewProfile/index';
 import UpdateProfile from './containers/Profile/UpdateProfile/index';
@@ -12,13 +17,23 @@ import UpdateProfile from './containers/Profile/UpdateProfile/index';
 export default () => (
   <Provider store={store}>
     <Router>
-      <Switch>
-        <Route exact path={ROUTES.home} component={Home} />
+      <div>
+        <Switch>
+          <Route exact path={ROUTES.home} component={Articles} />
 
-        <Route exact path={ROUTES.getUserProfile} component={ViewProfile} />
+          <Route exact path={ROUTES.success} component={Success} />
 
-        <Route exact path={ROUTES.updateUserProfile} component={UpdateProfile} />
-      </Switch>
+          <Route exact path={ROUTES.getUserProfile} component={ViewProfile} />
+
+          <Route exact path={ROUTES.updateUserProfile} component={UpdateProfile} />
+
+          <PrivateRoute exact path={ROUTES.newArticle} component={NewArticle} />
+
+          <Route exact path={ROUTES.article} component={MainArticle} />
+
+          <PrivateRoute exact path={ROUTES.editArticle} component={UpdateArticle} />
+        </Switch>
+      </div>
     </Router>
   </Provider>
 );
