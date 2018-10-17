1cad161d68feff6ac338b78486939f23f6f1c79c
rethes <esther.mutua@strathmore.edu>
2018-10-17 12:23:16 +0300

    [Feature #159965441] Update user authorization token
    Signed-off-by: rethes <esther.mutua@strathmore.edu>

--- src/containers/Profile/UpdateProfile/actions.js
+++ src/containers/Profile/UpdateProfile/actions.js
@@ -5,7 +5,6 @@ import {
   UPDATE_PROFILE_FAILURE, CLEAR_STORE,
 } from './ActionTypes';
 
-
 export const updateUserProfile = () => ({ type: UPDATE_USER_PROFILE_REQUEST });
 
 export const updateUserProfileSuccess = payload => ({
@@ -21,6 +20,7 @@ export const updateUserProfileFailure = errors => ({
 // update user details
 const updateUser = (profile) => {
   const url = 'https://ah-leagueoflegends-staging.herokuapp.com/api/user/';
+  const token = localStorage.getItem('access_token');
   return (dispatch) => {
     dispatch(updateUserProfile());
     return axios.put(url,
@@ -29,7 +29,7 @@ const updateUser = (profile) => {
         headers: {
           'Content-Type': 'application/json',
           Authorization:
-                'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MTgsImV4cCI6MTU0MjIxMzc0MX0.RoqZTO7S4ySw1bOzzBtLuV4y6wB4ChQl4-oiqqYy6hM',
+                `Bearer ${token}`,
         },
       },
     ).then(
