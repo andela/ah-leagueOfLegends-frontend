216c868aa3c93be10c986b771d1e9dff8f0ffd6b
rethes <esther.mutua@strathmore.edu>
2018-10-17 03:29:47 +0300

    [Feature #159965441] Remove App.js file from imports
    Signed-off-by: rethes <esther.mutua@strathmore.edu>

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/styles.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Routes from './routes';
 
