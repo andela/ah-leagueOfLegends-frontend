3dd6dfb7e1d96c072c2911731092e162e55c5645
Mutuba <daniel.mutuba@andela.com>
2018-10-18 12:03:07 +0300

    [Feature #159965443] Fix hound violations

--- src/components/Rating/Rating.js
+++ src/components/Rating/Rating.js
@@ -12,20 +12,19 @@ class ArticleRating extends Component {
 
   componentDidMount = () => {
     const { slug } = this.props;
+    // eslint-disable-next-line
     if (slug !== undefined) this.props.InitialRate(slug);
   };
 
   handleStarClick = (nextValue) => {
+    // eslint-disable-next-line
     this.props.rateArticle(this.props.slug, nextValue);
   };
 
   render() {
     return (
       <React.Fragment>
-        <div
-          className="row"
-          style={{ centerContent: 'flex-end', marginLeft: '20%' }}
-        >
+        <div className="row" style={{ centerContent: 'flex-end', marginLeft: '20%' }}>
           <div className="col s12 m6">
             <StarRatings
               name="rateArticle"
@@ -33,6 +32,7 @@ class ArticleRating extends Component {
               starRatedColor="green"
               starDimension="15px"
               rating={
+                // eslint-disable-next-line
                 this.props.ratingReducer.rating !== undefined ? this.props.ratingReducer.rating : 0
               }
               changeRating={this.handleStarClick}
@@ -43,8 +43,10 @@ class ArticleRating extends Component {
                   {' '}
                   Rating:
                   {' '}
-                  {this.props.ratingReducer.average_rating !== undefined
+                  {// eslint-disable-next-line
+                  this.props.ratingReducer.average_rating !== undefined
                     ? this.props.ratingReducer.average_rating.toFixed(1)
+
                     : this.props.average_rating}
                 </span>
               </span>
