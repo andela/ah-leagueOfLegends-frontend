07237f962234ed99c844eb9baa4ddadfc6eb955d
Teddykavooh <teddykavooh@gmail.com>
2018-10-19 14:34:10 +0300

    [Feature #159965447]Resolve hound violations

--- src/components/tags/index.js
+++ src/components/tags/index.js
@@ -1,24 +1,35 @@
 import React, { Component } from 'react';
-
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { searchItem } from '../../containers/Search/actions';
 
 class Tags extends Component {
   componentDidMount() {}
+
+  handleSearch = (e) => {
+    const { dispatch } = this.props;
+    console.log(this.props);
+    const searchText = e.target.getAttribute('data-value');
+    console.log(searchText);
+    const filter = 'tagList';
+    dispatch(searchItem(searchText, filter));
+  }
 
   render() {
     return (
       <div>
         <div className="">
           <ul className="tabs tabs-fixed-width tab-demo z-depth-0">
-            <li className="tab1"><a href="#test1">COLLECTIONS</a></li>
-            <li className="tab1"><a href="#test2">POWER TRIP</a></li>
-            <li className="tab1"><a href="#test3">CULTURE</a></li>
-            <li className="tab1"><a href="#test4">TECH</a></li>
-            <li className="tab1"><a href="#test0">STARTUPS</a></li>
-            <li className="tab1"><a href="#test0">SELF</a></li>
-            <li className="tab1"><a href="#test0">POLITICS</a></li>
-            <li className="tab1"><a href="#test0">DESIGN</a></li>
-            <li className="tab1"><a href="#test0">HEALTH</a></li>
-            <li className="tab1"><a href="#test0">POPULAR</a></li>
+            <li className="tab1"><a data-value="collections" onClick={this.handleSearch} href="#collections">COLLECTIONS</a></li>
+            <li className="tab1"><a data-value="power" onClick={this.handleSearch} href="#powertrip">POWER TRIP</a></li>
+            <li className="tab1"><a data-value="culture" onClick={this.handleSearch} href="#culture">CULTURE</a></li>
+            <li className="tab1"><a data-value="tech" onClick={this.handleSearch} href="#tech">TECH</a></li>
+            <li className="tab1"><a data-value="start" onClick={this.handleSearch} href="#startups">STARTUPS</a></li>
+            <li className="tab1"><a data-value="andela" onClick={this.handleSearch} href="#andela">ANDELA WAY</a></li>
+            <li className="tab1"><a data-value="politics" onClick={this.handleSearch} href="#politics">POLITICS</a></li>
+            <li className="tab1"><a data-value="design" onClick={this.handleSearch} href="#design">DESIGN</a></li>
+            <li className="tab1"><a data-value="health" onClick={this.handleSearch} href="#health">HEALTH</a></li>
+            <li className="tab1"><a data-value="popular" onClick={this.handleSearch} href="#popular">POPULAR</a></li>
           </ul>
         </div>
       </div>
@@ -26,4 +37,7 @@ class Tags extends Component {
   }
 }
 
-export default Tags;
+const mapDispatchToProps = dispatch => bindActionCreators({ search: searchItem },
+  dispatch);
+
+export default connect(mapDispatchToProps)(Tags);
