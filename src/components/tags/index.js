162ec642944e7adc3f992fa1d88f50dfaf88d096
Teddykavooh <teddykavooh@gmail.com>
2018-10-19 10:11:17 +0300

    [Feature #159965447]Refactor the tag tabs

--- src/components/tags/index.js
+++ src/components/tags/index.js
@@ -1,35 +1,29 @@
-import { React } from 'react';
+import React, { Component } from 'react';
 
-const Tags = () => (
-  <div>
-    <div className="row">
-      <div className="col s12">
-        <ul className="tabs">
-          <li className="tab col s3"><a href="#test1">COLLECTIONS</a></li>
-          <li className="tab col s3"><a href="#test2">POWER TRIP</a></li>
-          <li className="tab col s3"><a href="#test3">CULTURE</a></li>
-          <li className="tab col s3"><a href="#test4">TECH</a></li>
-          <li className="tab col s3"><a href="#test4">STARTUPS</a></li>
-          <li className="tab col s3"><a href="#test4">SELF</a></li>
-          <li className="tab col s3"><a href="#test4">POLITICS</a></li>
-          <li className="tab col s3"><a href="#test4">DESIGN</a></li>
-          <li className="tab col s3"><a href="#test4">HEALTH</a></li>
-          <li className="tab col s3"><a href="#test4">POPULAR</a></li>
-        </ul>
+
+class Tags extends Component {
+  componentDidMount() {}
+
+  render() {
+    return (
+      <div>
+        <div className="">
+          <ul className="tabs tabs-fixed-width tab-demo z-depth-0">
+            <li className="tab1"><a href="#test1">COLLECTIONS</a></li>
+            <li className="tab1"><a href="#test2">POWER TRIP</a></li>
+            <li className="tab1"><a href="#test3">CULTURE</a></li>
+            <li className="tab1"><a href="#test4">TECH</a></li>
+            <li className="tab1"><a href="#test0">STARTUPS</a></li>
+            <li className="tab1"><a href="#test0">SELF</a></li>
+            <li className="tab1"><a href="#test0">POLITICS</a></li>
+            <li className="tab1"><a href="#test0">DESIGN</a></li>
+            <li className="tab1"><a href="#test0">HEALTH</a></li>
+            <li className="tab1"><a href="#test0">POPULAR</a></li>
+          </ul>
+        </div>
       </div>
-      <div id="test1" className="col s12">Test 1</div>
-      <div id="test2" className="col s12">Test 2</div>
-      <div id="test3" className="col s12">Test 3</div>
-      <div id="test4" className="col s12">Test 4</div>
-      <div id="test4" className="col s12">Test 5</div>
-      <div id="test4" className="col s12">Test 6</div>
-      <div id="test4" className="col s12">Test 7</div>
-      <div id="test4" className="col s12">Test 8</div>
-      <div id="test4" className="col s12">Test 9</div>
-      <div id="test4" className="col s12">Test 10</div>
-    </div>
-  </div>
-);
-
+    );
+  }
+}
 
 export default Tags;
