c710522d2b9f759c61ebc1c6bdeac07c10c0633f
quantum-35 <quantumcomputing35@icloud.com>
2018-10-21 22:52:28 +0300

    --amend

--- src/containers/comments/getComents/displayComments.js
+++ src/containers/comments/getComents/displayComments.js
@@ -3,7 +3,6 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import PropTypes from 'prop-types';
 import Moment from 'react-moment';
-import M from 'materialize-css';
 
 import getComents from './actions';
 import publishComment from '../actions';
@@ -17,8 +16,6 @@ class DisplayComments extends PureComponent {
     if (articleSlug !== undefined) {
       allComment(articleSlug);
     }
-    const el = document.querySelectorAll('.dropdown-trigger');
-    M.Dropdown.init(el);
   }
 
   handleMouseEnter(index) {
@@ -27,13 +24,50 @@ class DisplayComments extends PureComponent {
     if (parseInt(document.getElementById(index).id) === parseInt(index)) {
       if (typeof show === 'number' && show !== index) {
         const div = document.getElementById(show);
-        div.classList.remove('anotherclass');
+        if (div) {
+          div.classList.remove('anotherclass');
+        }
       }
       const div = document.getElementById(index);
       this.setState({ show: index });
       div.classList.toggle('anotherclass');
       // eslint-disable-next-line
     }
+  }
+
+  editCommentHandler(index, display, commentId, articleSlug) {
+    const { manipulateComment, allComment } = this.props;
+    // eslint-disable-next-line
+    if (parseInt(document.getElementById(index).id) === parseInt(index)) {
+      const div = document.createElement('div');
+      div.className = 'foo';
+      div.id = index;
+      // eslint-disable-next-line
+      if (parseInt(div.id) === display) {
+        const div1 = document.getElementById(`comment-list${index}`);
+        div.classList.toggle('anotherclass');
+        div1.innerHTML += `
+        <div class="row" id="modal-edit">
+          <form class="col s12">
+          <i class="material-icons close-edit" onClick='{((document.getElementById("modal-edit").style.display="none") && ${allComment(this.articleSlug)} )}'>close</i>
+            <div class="row">
+              <div class="input-field col s12">
+                <textarea id="textarea1" class="materialize-textarea"></textarea>
+                <label for="textarea1">Update comment</label>
+              </div>
+            </div>
+          </form>
+        </div>
+        `;
+        // window.location.reload();
+        allComment(articleSlug);
+      }
+      allComment(articleSlug);
+    }
+  }
+
+  close() {
+    console.log("hello world.")
   }
 
   deleteCommentHandler(commentId, articleSlug) {
@@ -45,6 +79,7 @@ class DisplayComments extends PureComponent {
 
   render() {
     const { allComments, articleSlug } = this.props;
+    const { show } = this.state;
     let allComents;
     if (allComments.success && allComments) {
       try {
@@ -60,8 +95,11 @@ class DisplayComments extends PureComponent {
             </i>
 
             <ul className="comment-dropdown" id={index}>
-
-              <button type="submit" className="hoverable" onClick={() => this.deleteCommentHandler(comment.id, articleSlug)}>
+              <button type="submit" id="cmt-edit" className="hoverable" style={{ width: '100px' }} onClick={() => this.editCommentHandler(index, show, comment.id, articleSlug)}>
+                <i className="tiny material-icons">edit</i>
+                  Edit
+              </button>
+              <button type="submit" className="hoverable" onClick={() => this.deleteCommentHandler(comment.id, articleSlug)} style={{ width: '100px' }}>
                 <i className="tiny material-icons">delete</i>
                     Delete
               </button>
@@ -84,6 +122,7 @@ class DisplayComments extends PureComponent {
               </div>
             </div>
             <CommentsComponent comments={comment} />
+            <div id={`comment-list${index}`} />
           </div>
         ));
       } catch (e) {
