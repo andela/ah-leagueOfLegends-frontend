5df268c28dda8c0018fe090353e5017a19ac98b5
quantum-35 <quantumcomputing35@icloud.com>
2018-11-02 12:51:54 +0300

    --ammend

--- src/containers/Articles/Create/userInfo.test.js
+++ src/containers/Articles/Create/userInfo.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-// import store from '../../../store/store';
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import { UsrInfo } from './userInfo';
