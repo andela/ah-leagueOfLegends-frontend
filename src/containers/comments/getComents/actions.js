99c3fef4c3d221121a32a20f763761e1a2972744
quantum-35 <quantumcomputing35@icloud.com>
2018-11-02 12:53:11 +0300

    --ammend

--- src/containers/comments/getComents/actions.js
+++ src/containers/comments/getComents/actions.js
@@ -16,5 +16,3 @@ export const getComments = slug => (dispatch) => {
     .then(res => dispatch(commentSuccess(res)))
     .catch(err => dispatch(commentFailure(err)));
 };
-
-// export default getComments;
